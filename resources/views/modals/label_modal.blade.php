1b20421da0129221926bc5d7060f7facc381c5ca
Renato Lucena <cpdrenato@gmail.com>
2025-06-16 11:55:27 -0300

    [FIX] removendo arquivos nao utilizados

--- resources/views/modals/label_modal.blade.php
+++
@@ -1,9 +0,0 @@
-<div id="labelModal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <h2>Labels</h2>
-        <div id="labels_container">
-            <!-- Labels serão carregados aqui -->
-        </div>
-    </div>
-</div>
